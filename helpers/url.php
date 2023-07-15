18f19833b8fdaf5b5f2428bcd71967bdcf69a7e0
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-07-15 19:11:07 -0300

    first commit

---
+++ helpers/url.php
@@ -0,0 +1,4 @@
+<?php
+
+$port = '8282';
+$BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . ':' . $port;
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>Blog CecCode &copy; 2023</p>
+    </footer>
+</body>
+</html>

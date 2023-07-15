6791ec3c7e738b30fe06ffc564bc62f6c7d7cd21
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-07-15 19:34:31 -0300

    finish project

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,5 @@
 <?php
+    include_once('helpers/url.php');
     include_once('data/posts.php');
     include_once('data/categories.php');
 ?>
@@ -9,15 +10,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog CevCode</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
-        <a href="index.php" id="logo">
-            <img src="/img/logo.png" alt="Blog CevCode">
+        <a href="<?= $BASE_URL ?>index.php" id="logo">
+            <img src="<?= $BASE_URL ?>img/logo.png" alt="Blog CevCode">
         </a>
         <nav>
             <ul id="navbar">
